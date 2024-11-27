cd38700a6d27a208f41ec102ac323a584565c60b
Tepz <tayyab2607@gmail.com>
2024-11-27 20:33:07 +0100

    Update

--- config/i3blocks/scripts/battery/battery.sh
+++ config/i3blocks/scripts/battery/battery.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+files=$(ls /sys/class/power_supply | wc -l)
 capacity=$(cat /sys/class/power_supply/BAT0/capacity)
 status=$(cat /sys/class/power_supply/BAT0/status)
+
+if [[ $files -eq 0 ]];then
+	nline=$(cat config | grep -n "\[battery\]" | cut -d : -f 1)
+	for i in {0..2};do
+		sed -i "${nline}s/.*/#&/" ~/.config/i3blocks/config
+		nline=$((nline + 1))
+	done
+	i3-msg restart
+fi
 
 if [[ $capacity -ge 75 ]] && [[ $capacity -lt 100 ]];then
 	echo " $capacity%"
