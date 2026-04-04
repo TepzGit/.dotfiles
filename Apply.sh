3b66f3ecfae04a854ee887bfed82aa7674cb780b
Tepz <Tepz@Tepz.com>
2026-04-05 00:53:45 +0200

    Update

--- Apply.sh
+++ Apply.sh
@@ -1,13 +1,33 @@
 #!/usr/bin/bash
 
-blacklist=(".git" "$1")
+blacklist=(".git" "$1" "." "..")
 for dir in *;do
 	blackisted=false
 	for item in "${blacklist[@]}";do
-		if [ "item" == dir ];then
-			continue
+		if [ "$item" == "$dir" ];then
+			blackisted=true
+			break
 		fi
 	done
+	if [ blackisted == true ];then
+		continue
+	fi
 	
-	cp -r $dir ~/.
+	cp -r "$dir" ~/.
+done
+
+
+for dir in .*;do
+	blackisted=false
+	for item in "${blacklist[@]}";do
+		if [ "item" == "$dir" ];then
+			blackisted=true
+			break
+		fi
+	done
+	if [ blackisted == true ];then
+		continue
+	fi
+	
+	cp -r "$dir" ~/.
 done
